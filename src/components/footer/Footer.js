d05500174d2cc1c7a6ba1f7cdc3d8310affd826c
Achraf Bouanani <ashrafbouanani@gmail.com>
2022-06-14 22:56:03 +0100

    test

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ class Header extends React.Component{
     
     render() {
         return(
-        <footer className="py-3 border-top border-bottom" ><center><small> <b>Kwiz</b> - Copyright ©2019  | <a href="https://takentechs.com">Takentechs</a></small></center></footer>
+        <footer className="py-3 border-top border-bottom" ><center><small> <b>Kwiz</b> - Copyright ©2019 - 2022  | <a href="https://github.com/AchrafB94">Achraf Bouanani</a></small></center></footer>
         
 
         )
