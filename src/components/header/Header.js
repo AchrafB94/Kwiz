e283e71534e7d6a2e25406873b81fc8c63987091
Achraf Bouanani <ashrafbouanani@gmail.com>
2022-07-09 18:05:42 +0100

    netlify deployment

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,103 +1,66 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link, withRouter } from 'react-router-dom'
-import {Navbar,} from 'react-bootstrap'
-import Logo from '../../images/logo.png'
- 
-/*
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, withRouter } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import Logo from "../../images/logo.png";
 
- Nav, OverlayTrigger, Popover
-const popover = (
-  <Popover id="popover-basic" title="Notifications">
-    And here's some <strong>amazing</strong> content. It's very engaging. right?
-  </Popover>
-);
-
-
-const Example = () => (
-  <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-     <div className="nav-link"><FontAwesomeIcon icon="bell" size="lg"    /></div>
-  </OverlayTrigger>
-);
-*/
-class Header extends React.Component{
- 
-
-
+class Header extends React.Component {
   logOut(e) {
-    e.preventDefault()
-    localStorage.removeItem('usertoken')
-    localStorage.removeItem('userlevel')
-    window.location.replace('/login')
-
-
-}
-
-
-
-
-    render() {
-      const loginRegLink = (
-        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li className="nav-item">
-                <Link to="/register" className="nav-link">
-                <FontAwesomeIcon icon="plus" size="lg" /> S'inscrire
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/login" className="nav-link">
-                <FontAwesomeIcon icon="sign-in-alt" size="lg"/> Se connecter
-                </Link>
-            </li>
-
-        </ul>
-    )
+    e.preventDefault();
+    localStorage.removeItem("usertoken");
+    localStorage.removeItem("userlevel");
+    window.location.replace("/login");
+  }
+  render() {
+    const loginRegLink = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            <FontAwesomeIcon icon="plus" size="lg" /> S'inscrire
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            <FontAwesomeIcon icon="sign-in-alt" size="lg" /> Se connecter
+          </Link>
+        </li>
+      </ul>
+    );
     const userLink = (
-        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/profile" className="nav-link">
+            <FontAwesomeIcon icon="user" /> Mon Profile
+          </Link>
+        </li>
 
-            <li className="nav-item">
-                <Link to="/profile" className="nav-link">
-                <FontAwesomeIcon icon="user" /> Mon Profile
-                </Link>
-            </li>
-            
-            <li className="nav-item">
-                <Link to="/settings/profile" className="nav-link">
-                <FontAwesomeIcon icon="cog" /> Paramètres
-                </Link>
-            </li>
-            <li className="nav-item">
-                <a href="/" onClick={this.logOut.bind(this)} className="nav-link">
-                <FontAwesomeIcon icon="sign-out-alt" /> Se deconnecter
-                </a>
-            </li>
-        </ul>
-    )
-
-
-        return(
-<div>
-<Navbar bg="light" expand="lg" className="border-bottom">
-<Link className="navbar-brand" to="/">
-  <img src={Logo} alt="logo" height="30" style={{width: "10rem"}} />
-
-</Link>
-<ul className="navbar-nav ">
-            </ul>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    {localStorage.usertoken ? userLink : loginRegLink}
-    
-  
-  </Navbar.Collapse>
-</Navbar>
-</div>
-
-        )
-
-    }
-    
-
+        <li className="nav-item">
+          <Link to="/settings/profile" className="nav-link">
+            <FontAwesomeIcon icon="cog" /> Paramètres
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a href="/" onClick={this.logOut.bind(this)} className="nav-link">
+            <FontAwesomeIcon icon="sign-out-alt" /> Se deconnecter
+          </a>
+        </li>
+      </ul>
+    );
+    return (
+      <div>
+        <Navbar bg="light" expand="lg" className="border-bottom">
+          <Link className="navbar-brand" to="/">
+            <img src={Logo} alt="logo" height="30" style={{ width: "10rem" }} />
+          </Link>
+          <ul className="navbar-nav "></ul>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {localStorage.usertoken ? userLink : loginRegLink}
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Header);
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -8,84 +8,158 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import jwt_decode from "jwt-decode";
 import { Link } from "react-router-dom";
 
-
-
-
-
 class Sidebar extends React.Component {
-
-
   state = {
-    selected: 2
-
-  }
-
-  
+    selected: 2,
+  };
 
   componentDidMount() {
-
-      this.props.getSubjects()
-    
-    
+    this.props.getSubjects();
   }
 
-  checkPermission(permissions,ruleId) {
-    const found = permissions.some(el => el.ruleId === ruleId);
-    if (found) return true
-    else return false
+  checkPermission(permissions, ruleId) {
+    const found = permissions.some((el) => el.ruleId === ruleId);
+    if (found) return true;
+    else return false;
   }
-
 
   render() {
-    if(localStorage.usertoken) {
+    if (localStorage.usertoken) {
       const decoded = jwt_decode(localStorage.usertoken);
-     
-     const permissions = decoded.role.permissions;
-     
-    
-    return (
-      <div className="border-right" id="sidebar-wrapper">
-          <div className="list-group  list-group-flush" >
 
-       <div>
-      {this.checkPermission(permissions,5) ? <Link  to={"/admin/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="cogs" /> Administration</Link>: ""}
-       {this.checkPermission(permissions,7) ? <Link  to={"/admin/users/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="user" /> Utilisateurs</Link>: ""}
-       {this.checkPermission(permissions,9) ? <Link  to={"/admin/schools/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="school" /> Etablissements</Link>: ""}
-      {this.checkPermission(permissions,1) ?  <Link  to={"/admin/levels/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="plus" /> Niveaux</Link>: ""}
-       {this.checkPermission(permissions,2) ? <Link  to={"/admin/subjects/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="book" /> Matiéres</Link>: ""}
-       {this.checkPermission(permissions,3) ? <Link  to={"/admin/quiz/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="clipboard-list" /> Quizz</Link> : ""}
-       {this.checkPermission(permissions,6) ? <Link  to={"/admin/scores/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="clipboard-check" /> Scores</Link>: ""}
-  
-       {this.checkPermission(permissions,4) ?  <Link  to={"/contrib/"} id="admin" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="question-circle" /> Mes Quizz</Link>: ""}
-       </div>
+      const permissions = decoded.role.permissions;
 
-    </div>
-        <div className="list-group  list-group-flush" >
-        {this.props.subjects.map((subject) => {
       return (
-        <div key={subject.id} >
-          <Link  to={"/subject/" + subject.id} id="subject" className="list-group-item list-group-item-action" >{subject.name}</Link>
+        <div className="border-right" id="sidebar-wrapper">
+          <div className="list-group  list-group-flush">
+            <div>
+              {this.checkPermission(permissions, 5) ? (
+                <Link
+                  to={"/admin/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="cogs" /> Administration
+                </Link>
+              ) : (
+                ""
+              )}
+              {this.checkPermission(permissions, 7) ? (
+                <Link
+                  to={"/admin/users/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="user" /> Utilisateurs
+                </Link>
+              ) : (
+                ""
+              )}
+              {this.checkPermission(permissions, 9) ? (
+                <Link
+                  to={"/admin/schools/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="school" /> Etablissements
+                </Link>
+              ) : (
+                ""
+              )}
+              {this.checkPermission(permissions, 1) ? (
+                <Link
+                  to={"/admin/levels/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="plus" /> Niveaux
+                </Link>
+              ) : (
+                ""
+              )}
+              {this.checkPermission(permissions, 2) ? (
+                <Link
+                  to={"/admin/subjects/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="book" /> Matiéres
+                </Link>
+              ) : (
+                ""
+              )}
+              {this.checkPermission(permissions, 3) ? (
+                <Link
+                  to={"/admin/quiz/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="clipboard-list" /> Quizz
+                </Link>
+              ) : (
+                ""
+              )}
+              {this.checkPermission(permissions, 6) ? (
+                <Link
+                  to={"/admin/scores/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="clipboard-check" /> Scores
+                </Link>
+              ) : (
+                ""
+              )}
+
+              {this.checkPermission(permissions, 4) ? (
+                <Link
+                  to={"/contrib/"}
+                  id="admin"
+                  className="list-group-item list-group-item-action"
+                >
+                  {" "}
+                  <FontAwesomeIcon icon="question-circle" /> Mes Quizz
+                </Link>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-      )})}
-    </div>
-    <div className="list-group  list-group-flush" >
-    <Link  to={"/forum/"} id="forum" className="list-group-item list-group-item-action" > <FontAwesomeIcon icon="comment-alt" /> 2K-PI</Link>
-  </div>
-      </div>
-    )}else return null
+          <div className="list-group  list-group-flush">
+            {this.props.subjects.map((subject) => {
+              return (
+                <div key={subject.id}>
+                  <Link
+                    to={"/subject/" + subject.id}
+                    id="subject"
+                    className="list-group-item list-group-item-action"
+                  >
+                    {subject.name}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    } else return null;
   }
 }
 
 Sidebar.propTypes = {
   subjects: PropTypes.array.isRequired,
-  getSubjects: PropTypes.func.isRequired
+  getSubjects: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  subjects: state.subjects.subjects
+const mapStateToProps = (state) => ({
+  subjects: state.subjects.subjects,
 });
 
-export default connect(
-  mapStateToProps,
-  { getSubjects }
-)(Sidebar);
+export default connect(mapStateToProps, { getSubjects })(Sidebar);
